57524d7ce8570dd798e3a5681f34a42810eaca5c
Vineeth Rai K <vineethraik23082000@example.com>
2023-02-21 19:27:00 +0530

    Change App component location

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+// import {database} from './../../database/database.js'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Util from './../Util/Util'
+
+function App() {
+  return (
+  );
+}
+
+export default App;
