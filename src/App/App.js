1c67c9afda571654ddd55096c6d8b1490a9695fe
Vineeth Rai K <vineethraik23082000@example.com>
2023-02-23 14:10:53 +0530

    Import _Routes and Remove junk Imports

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 // import {database} from './../../database/database.js'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Signup from "./../modules/Signup/Signup";
+import _Routes from "./../routes";
 
 function App() {
   return (
