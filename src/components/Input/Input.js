b1b396fbe44ecc15e988d968c30a6c0986b4e2fc
Vineeth Rai K <vineethraik23082000@example.com>
2023-02-27 16:56:25 +0530

    Modify Input component to handle ckeckbox

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -8,7 +8,8 @@ function Input({id, value,type, setOnChange, error , label, className}){
             <div className={`input ${className}`}>
               <label htmlFor={id}>{label}</label>
               {(type === 'file')? <input id={id} value={value} onChange={(e)=>{setOnChange(e.target.files[0])}} type={type} /> 
-              : <input  id={id} value={value} onChange={(e)=>{setOnChange(e.target.value)}} type={type} /> }
+              :(type === 'checkbox')? <input  id={id} checked={value} onChange={(e)=>{setOnChange(e.target.value)}} type={type} />
+          :<input  id={id} value={value} onChange={(e)=>{setOnChange(e.target.value)}} type={type} /> }
             </div>
             {(error)? (<p className="error">{error}</p>): ''}
           </div>
